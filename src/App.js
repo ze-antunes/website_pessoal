52e332765c1cee951eaea2e12b26162935ff7857
ze-antunes <jprantunes2000@gmail.com>
2022-12-20 12:57:21 +0000

    add music files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import bgMusic from './assets/sounds/kronos_unveiled.mp3'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Ola Funciona</h1>
+        <audio controls loop>
+          <source src={bgMusic} />
+        </audio>
+      </div>
+    </Router>
   );
 }
 
